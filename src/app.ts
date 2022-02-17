34359250446c89e60ec8abf816934aa34bbd0306
Jun Guo <gjuoun@gmail.com>
2022-02-17 10:33:40 -0400

    add demo findmany

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
-import express from 'express'
+import express, { ErrorRequestHandler } from 'express'
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
 
 const app = express()
 
@@ -14,7 +16,20 @@ app.get('/', (req, res) => {
   } as Response)
 })
 
+app.get('/users',async (req,res) => {
+  const users = await prisma.user.findMany()
+
+  res.send(users)
+})
+
+
+const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!')
+}
+
+app.use(errorHandler)
+
 app.listen(3000, () => {
   console.log("running on port 3000");
-
 })
